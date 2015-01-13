51cb3dc952d51c7daced36fa887829022ea1be7a
Prashant Chaudhari <prashant@jombay.com>
2015-01-13 13:02:12 +0530

    renamed to node-active-resource

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var NodeVger = module.exports = function(options) {
+var NodeActiveResource = module.exports = function(options) {
   module.exports.config = options;
   var Resource = require("./lib/resource");
   module.exports.Resource = Resource;
