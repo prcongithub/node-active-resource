f167b1dce55acbc50a659f56b768f96c58e603b5
Prashant Chuadhari <prashant.chaudhari89@gmail.com>
2019-01-22 19:05:10 +0530

    support for multiple apis

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var NodeActiveResource = module.exports = function(options) {
   module.exports.config = options;
-  var Resource = require("./lib/resource");
+  var Resource = require("./lib/resource")(options);
   module.exports.Resource = Resource;
 };
 
