6f3075cbe7950afe452a7828c29317ce6fc0ef0f
Prashant Chaudhari <prashant.chaudhari89@gmail.com>
2019-05-24 10:17:41 +0530

    use latest updates and fix vuln for underscore

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var NodeActiveResource = module.exports = function(options) {
   module.exports.config = options;
-  var Resource = require("./lib/resource");
+  var Resource = require("./lib/resource")(options);
   module.exports.Resource = Resource;
 };
-
-
-
-
